48621b5cee4a4c569d830f8279c1cf9fa6422b7d
Tina Ostrander <tostrander@greenriver.edu>
2024-04-09 13:42:33 -0400

    Add home page view

--- index.php
+++ index.php
@@ -16,7 +16,11 @@ $f3 = Base::instance();
 // Define a default route
 // https://tostrander.greenriverdev.com/328/hello-fat-free/
 $f3->route('GET /', function() {
-    echo '<h1>Hello Fat-Free!</h1>';
+    //echo '<h1>Hello Fat-Free!</h1>';
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 // Run Fat-Free
